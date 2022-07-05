4f9069d41d1c48730d457cdf3d7f46db550d4df5
Kanon-Hosen <freelancerkanon1@gmail.com>
2022-07-05 11:48:13 +0600

    this is first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const navSlide = () =>{
+    const nav = document.querySelector('.nav-links');
+    const burger = document.querySelector('.burger');
+    const navlinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener( 'click',()=>{
+        nav.classList.toggle('nav-active');
+
+        //animation nav links
+        navlinks.forEach((link , index)=>{
+            if(link.style.animation){
+                link.style.animation = ''
+            }
+            else{
+                link.style.animation = `navlinksFade 0.5s ease forwards ${index / 7 * 1.5}s`;
+            }
+
+        });
+        burger.classList.toggle('toggle');
+    });
+}
+navSlide();
